ce0d63f44b519654ed49b6765a7dc6c9c6143f1a
Jack Reuter <djreuter45@gmail.com>
2017-12-05 19:40:23 -0700

    added welcome view content and image

--- resources/views/layouts/welcome.blade.php
+++ resources/views/layouts/welcome.blade.php
@@ -2,12 +2,19 @@
 
 @section('content')
 <div class="container">
-<h1>Welcome To Deep Dive Tutor</h1>
+<h1 class="heading">Welcome To Deep Dive Tutor</h1>
 <hr/>
+<div class="row">
+  <div class="col welcome">
+    <div class="card card-image">
+      <img class="logo" src="{{ asset('images/ddt-resize.png') }}">
+    </div><!-- /.card -->
+  </div><!-- /.col -->
+</div><!-- /.row -->
   <div class="row">
-    <div class="col">
-      <div class="card">
-        <p>
+    <div class="col welcome">
+      <div class="card card-text">
+        <p class="welcome-text">
           Readymade four loko austin umami cronut organic freegan tattooed selfies hashtag hexagon neutra butcher kale chips. Cliche cray humblebrag, tote bag banjo literally hoodie taiyaki occupy. Gastropub selfies tumeric hammock aesthetic. Lo-fi shoreditch keytar cornhole microdosing whatever blog narwhal kickstarter pour-over hella brooklyn. Banh mi food truck shoreditch post-ironic. Schlitz listicle hoodie fingerstache poke lo-fi. Keytar mustache air plant pork belly swag, yuccie coloring book tilde distillery poutine food truck pinterest offal asymmetrical.
         </p>
       </div><!-- /.card -->
