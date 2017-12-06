4a1bcd807f7f70005fa329e60e86ec9fd72e65af
Jack Reuter <djreuter45@gmail.com>
2017-12-05 18:46:06 -0700

    updated routes and added a welcome view

---
+++ resources/views/layouts/welcome.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="container">
+<h1>Welcome To Deep Dive Tutor</h1>
+<hr/>
+  <div class="row">
+    <div class="col">
+      <div class="card">
+        <p>
+          Readymade four loko austin umami cronut organic freegan tattooed selfies hashtag hexagon neutra butcher kale chips. Cliche cray humblebrag, tote bag banjo literally hoodie taiyaki occupy. Gastropub selfies tumeric hammock aesthetic. Lo-fi shoreditch keytar cornhole microdosing whatever blog narwhal kickstarter pour-over hella brooklyn. Banh mi food truck shoreditch post-ironic. Schlitz listicle hoodie fingerstache poke lo-fi. Keytar mustache air plant pork belly swag, yuccie coloring book tilde distillery poutine food truck pinterest offal asymmetrical.
+        </p>
+      </div><!-- /.card -->
+    </div><!-- /.col -->
+  </div><!-- /.row -->
+</div><!-- /.container -->
+
+@endsection
